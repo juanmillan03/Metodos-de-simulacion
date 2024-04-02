088e239b7cf050c6f1a15590f830429d71c7b2f4
juanmillan03 <juanesmilland@gmail.com>
2024-04-02 00:30:44 -0500

    punto_1

--- Taller1/prunto1/grafica_C.py
+++ Taller1/prunto1/grafica_C.py
@@ -4,21 +4,18 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-
-
 def Punto_C(data_path):
     datos=pd.read_csv(data_path,sep=" " ,names=['t','s',"i","r"])
-    datos_rales=pd.read_csv("Datos_reales.txt",sep="\t" ,names=['t',"i"])
+    datos_rales=pd.read_csv("Datos_reales.txt",sep="\t" ,names=['t',"i"],skiprows=1)
     fig, ax = plt.subplots( )
-    ax.plot(datos_rales['t'],datos_rales['i'],label="Reales", linestyle='-.')
+    ax.scatter(datos_rales['t'],datos_rales['i'],label="Reales",s=1,c="r")
     ax.plot(datos['t'],datos['i'],label="Simulados", linestyle='-.')
+    ax.set_xticks(np.linspace(0, 300,8))
     ax.set_xlabel("Tiempo(Díaz)")
     ax.set_ylabel("Infectados ")
     ax.grid()
     ax.legend()
-    plt.title("SIR sin muertes ni nacimentos")
     plt.savefig("Modelosir_C.pdf")
-
 
 
     
